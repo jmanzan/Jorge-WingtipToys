8ad664dbef8a6a1312e32b143239aa874b35ee9f
jmanzanero <jmanzanero@ctdi.com>
2018-06-29 12:04:39 -0500

    Step3

--- WingtipToys/Site.Master.cs
+++ WingtipToys/Site.Master.cs
@@ -7,6 +7,8 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -71,7 +73,12 @@ namespace WingtipToys
         {
 
         }
-
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
         {
             Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
